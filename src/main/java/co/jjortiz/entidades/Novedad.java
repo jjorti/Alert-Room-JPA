971bc4e3fd5507392f87c3dc0307bf6cdde9f021
juanjortiz <jjortizsanta97@gmail.com>
2020-10-27 16:01:12 -0500

    Clase novedad con relaciones, a espera de revision

--- src/main/java/co/jjortiz/entidades/Novedad.java
+++ src/main/java/co/jjortiz/entidades/Novedad.java
@@ -2,11 +2,16 @@ package co.jjortiz.entidades;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -25,19 +30,24 @@ public class Novedad {
 	@Column(name = "idnovedad")
 	private Integer idNovedad;
 	
-	@Column(name = "idambiente",nullable = false, columnDefinition="CHAR(11)" )
-	private String idAmbiente;
+	//TODO Preguntar por el cascade
+	@ManyToOne
+	@JoinColumn(name = "ambiente_id", referencedColumnName = "idambiente", foreignKey = @ForeignKey(name ="Fk_novedad_ambiente"))
+	private Ambiente idAmbiente;
 	
-	@Column(name = "idusuarioI", nullable = false, columnDefinition="CHAR(11)" )
-	private String idUsuarioInstructor;
 	
 	//TODO Preguntar por tipo de dato que almacene hora y fecha 
     @Column(name = "fecha_hora")
     @Temporal(TemporalType.TIMESTAMP)
     private Date fechaHora;
     
-	@Column(name = "idusuarioG", nullable = false, columnDefinition="CHAR(11)" )
-	private String idUsuarioGuarda;
+    @ManyToOne
+    @JoinColumn(name = "idUsuarioI", referencedColumnName = "identificacion", foreignKey = @ForeignKey(name ="Fk_novedad_usuario_instructor"))
+	private Usuario idUsuarioInstructor;
+    
+    @ManyToOne
+    @JoinColumn(name = "idUsuarioG", referencedColumnName = "identificacion", foreignKey = @ForeignKey(name ="Fk_novedad_usuario_guarda"))
+	private Usuario idUsuarioGuarda;
     
     @Column(name ="observaciones", nullable = false , length = 255)
     private String observaciones;
@@ -58,15 +68,39 @@ public class Novedad {
      * @param idUsuarioGuarda
      * @param observaciones
      */
-    public Novedad(Integer idNovedad, String idAmbiente, String idUsuarioInstructor, Date fechaHora,
-			String idUsuarioGuarda, String observaciones) {
+	public Novedad(Integer idNovedad, Ambiente idAmbiente, Date fechaHora, Usuario idUsuarioInstructor,
+			Usuario idUsuarioGuarda, String observaciones) {
 		super();
 		this.idNovedad = idNovedad;
 		this.idAmbiente = idAmbiente;
-		this.idUsuarioInstructor = idUsuarioInstructor;
 		this.fechaHora = fechaHora;
+		this.idUsuarioInstructor = idUsuarioInstructor;
 		this.idUsuarioGuarda = idUsuarioGuarda;
 		this.observaciones = observaciones;
+	}
+
+	public Ambiente getIdAmbiente() {
+		return idAmbiente;
+	}
+
+	public void setIdAmbiente(Ambiente idAmbiente) {
+		this.idAmbiente = idAmbiente;
+	}
+
+	public Usuario getIdUsuarioInstructor() {
+		return idUsuarioInstructor;
+	}
+
+	public void setIdUsuarioInstructor(Usuario idUsuarioInstructor) {
+		this.idUsuarioInstructor = idUsuarioInstructor;
+	}
+
+	public Usuario getIdUsuarioGuarda() {
+		return idUsuarioGuarda;
+	}
+
+	public void setIdUsuarioGuarda(Usuario idUsuarioGuarda) {
+		this.idUsuarioGuarda = idUsuarioGuarda;
 	}
 
 	public Integer getIdNovedad() {
@@ -77,36 +111,12 @@ public class Novedad {
 		this.idNovedad = idNovedad;
 	}
 
-	public String getIdAmbiente() {
-		return idAmbiente;
-	}
-
-	public void setIdAmbiente(String idAmbiente) {
-		this.idAmbiente = idAmbiente;
-	}
-
-	public String getIdUsuarioInstructor() {
-		return idUsuarioInstructor;
-	}
-
-	public void setIdUsuarioInstructor(String idUsuarioInstructor) {
-		this.idUsuarioInstructor = idUsuarioInstructor;
-	}
-
 	public Date getFechaHora() {
 		return fechaHora;
 	}
 
 	public void setFechaHora(Date fechaHora) {
 		this.fechaHora = fechaHora;
-	}
-
-	public String getIdUsuarioGuarda() {
-		return idUsuarioGuarda;
-	}
-
-	public void setIdUsuarioGuarda(String idUsuarioGuarda) {
-		this.idUsuarioGuarda = idUsuarioGuarda;
 	}
 
 	public String getObservaciones() {
