de21c0d165acf6b7708a0623b2282526fdcc6df5
juanjortiz <jjortizsanta97@gmail.com>
2020-10-27 16:00:49 -0500

    Clase concepto con relaciones, a espera de revision

--- src/main/java/co/jjortiz/entidades/Concepto.java
+++ src/main/java/co/jjortiz/entidades/Concepto.java
@@ -1,10 +1,14 @@
 package co.jjortiz.entidades;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -26,6 +30,10 @@ public class Concepto {
 	
 	@Column(name = "aplica", nullable = false)
 	private Integer aplica;
+	
+	@OneToMany(mappedBy = "concepto", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	private List<Solicitud> listaSolicitudes;
+
 	
 	/**
 	 * Class constructor empty
